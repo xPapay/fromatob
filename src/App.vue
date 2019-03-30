79ae4afe08f9aaf03d5ca4aa8b9542b6978018fc
xPapay <papay.lukas@gmail.com>
2019-03-30 21:19:36 +0100

    Dynamically load footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
+  <div ref="container">
     <the-navigation />
     <router-view />
+    <div class="placeholder" style="height: 2000px;"></div>
   </div>
 </template>
 
 <script>
+  import { throttle } from 'lodash'
+  import Vue from 'vue'
   import TheNavigation from '@/components/TheNavigation'
     export default {
+        name: 'app',
         components: {
           TheNavigation,
+        },
+        mounted() {
+          const treshold = document.body.scrollHeight / 2
+          const viewportHeight = window.innerHeight
+          const throttled = throttle(() => {
+            const scrollTop = document.documentElement.scrollTop
+            const shouldLoad = scrollTop + viewportHeight >= treshold
+            if (shouldLoad) {
+              document.removeEventListener('scroll', throttled)
+              console.log('loading...')
+              import(/* webpackChunkName: "footer" */ '@/components/TheFooter')
+              .then(({ default: footer }) => {
+                const FooterClass = Vue.extend(footer)
+                const FooterComponent = new FooterClass()
+                FooterComponent.$mount()
+                this.$refs.container.appendChild(FooterComponent.$el)
+              })
+            }
+          }, 800)
+
+          document.addEventListener('scroll', throttled)
         }
     }
 </script>
--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -6,6 +6,11 @@
 
 <script>
     export default {
+        data() {
+            return {
+                myFooterData: 'bla bla'
+            }
+        }
         
     }
 </script>
